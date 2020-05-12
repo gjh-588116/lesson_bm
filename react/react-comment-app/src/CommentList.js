3cc7fc10635aee64b53cca4606327da0246841c9
gjh-588116 <2503797793@qq.com>
2020-05-12 21:26:53 +0800

    'react第二课'

---
+++ react/react-comment-app/src/CommentList.js
@@ -0,0 +1,13 @@
+import React ,{Component}from 'react';
+
+class CommentList extends Component{
+    render(){ // 接口
+        return(
+            <div>
+                CommentList
+            </div>
+        )
+    }
+}
+
+export default CommentList;
