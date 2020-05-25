91261fe44d11bd650641a506c6e0cf7722f109e9
gjh-588116 <2503797793@qq.com>
2020-05-25 20:13:40 +0800

    '闭包的知识'

--- js-closure闭包/scope.js
+++ js-closure闭包/scope.js
@@ -1,10 +1,19 @@
 var name = 'xiaoli';
 
 function showName(){
+    var name2 = 'lihua';
     console.log(name);
+    function add(){
+        console.log('嵌套',name2);
+    }
+    add();
 }
-
+showName();
 {
     var name1 = 'lilei';
     name = 'lihua';
+    {
+
+    }
 }
+console.log(name1);
