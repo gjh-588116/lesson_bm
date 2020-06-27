86b46cf99db155b722c41dfb6068938048099ed6
gjh-588116 <2503797793@qq.com>
2020-06-27 22:23:06 +0800

    'redux-状态管理'

---
+++ redux/shopping-cart/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import {Pro}
+import reducer from './reducers';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+const store = create
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
